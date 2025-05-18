d575898312a861fc1af014c6e22e083ef25dabba
Viviann09 <viviantan0910@gmail.com>
2025-05-18 13:25:56 +0700

    membuat header dan footer

---
+++ client/src/customer/pages/homepage.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <main className="container-fluid p-0">
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
