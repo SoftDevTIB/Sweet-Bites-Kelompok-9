dbcd6c25f93f4aa93f6cb16c459da0958aacc444
angel0060 <angelica.535230060@stu.untar.ac.id>
2025-06-16 15:51:16 +0700

    Coba fix error login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('../models/User').default; 
+const User = require('../models/User'); 
 const router = express.Router();
 const { verifyToken } = require('../middlewares/authMiddleware');
 
